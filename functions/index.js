f43d49b169b166f0d5ab2f97e5daff09c494cae3
Brett Anda <brettanda12@gmail.com>
2020-05-05 15:27:17 -0600

    Removed unused files

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 module.exports = {
   capitalize:         require("./capitalize"),
   random:             require("./random"),
-  parseTrainingData:  require("./parseTrainingData"),
-  trainClassifier:    require("./trainClassifier"),
   embed:              require("./embed"),
   hasDups:            require("./hasDups"),
   queryDialogFlow:    require("./queryDialogflow"),
