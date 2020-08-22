c05585314822375bc9e109702880f35f2734e1cf
Brett Anda <brettanda12@gmail.com>
2020-08-22 15:27:11 -0600

    fixed the ignore text

--- chat/ignoreText.js
+++ chat/ignoreText.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const text = fs.readFileSync('./ignoreChatsStartingWith.txt','utf-8');
-var textByLine = text.split("\r")
+var textByLine = text.split("\n")
 
 module.exports = msg => {
     textByLine.map((line,i) => {
-        textByLine[i] = line.replace("\n","");
+        textByLine[i] = line.replace("\r","");
     });
     return textByLine.filter(line => msg.cleanContent.startsWith(line));
 }
