8a817745a5ed36e3bc64dabba8f373abfd2cc8ac
Brett Anda <brettanda12@gmail.com>
2020-08-27 16:50:46 -0600

    Catchup on the dice roll

--- commands/diceRoll.js
+++ commands/diceRoll.js
@@ -1,9 +1,13 @@
 const func = require("../functions/");
+const { delMSGtimeout } = require("../config.json");
+
+// TODO: this rolls the dice but not the **2**d20 number of time the dice should be rolled
 
 module.exports = {
   name: "d",
   aliases: ["dice"],
   hidden: true,
+  owner: true,
   description: "D&D dice rolling",
   usage: "[Dice face count]+[Dice modifier]",
   execute(msg,args,bot) {
@@ -17,16 +21,19 @@ module.exports = {
     diceRolls.map(() => {
       const roll = resp.match(/([0-9]+)*d[0-9]+/);
 
-      console.log(roll);
+      // console.log(roll);
       const max = roll[0].match(/(?:d)([0-9]+)/)[1];
-      console.log(max);
+      // console.log(max);
       const newRoll = func.random(1,max);
 
-      resp = resp.replace(roll[0],newRoll);
+      resp = resp.replace(roll[0],`(${newRoll})`);
       
     })
-    console.log(resp);
-    console.log(eval(resp));
+    const math = resp;
+    resp = eval(resp);
+
+    msg.channel.send(func.embed(`You Rolled: **${resp}**`,"#fff","",msg.author,"","","",args.join(" "),`\nOriginal: '${math}'\n\n=**${resp}**`)).then(status => status.delete({timeout: delMSGtimeout * 20}));
+
     // !20d20+3*6/3
     // (([0-9]+)*d[0-9]+)
     // (?:(\d+)*d(\d+))+([0-9\+\/\*\-]+)
